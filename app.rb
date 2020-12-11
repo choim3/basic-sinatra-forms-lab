f96a5896216eab19a62f2aaf111f5e5486a6c2ac
choim3 <choim3@spu.edu>
2020-12-10 16:18:55 -0800

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello world"
+  end
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    erb :team
+  end
 end
